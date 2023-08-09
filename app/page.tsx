d0a5ba1e76ffb31159ad089440d3d91b4c50f2d0
Zachary Robinson <me@zsrobinson.com>
2023-08-09 17:14:01 -0400

    install shadcn/ui and create layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <main className="m-8">
-      <h1 className="text-2xl font-bold">Frontmatter Explorer</h1>
+    <main className="flex-grow">
+      <p>Here is some main content</p>
     </main>
   );
 }
